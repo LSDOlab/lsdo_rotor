5259aa490e6a737b7627cac29503a278511feaf1
MariusLRuh <mruh@ucsd.edu>
2023-03-01 18:34:21 -0800

    remove sympy imports for BEM/BILD

--- lsdo_rotor/core/BEM/functions/get_BEM_rotor_dictionary.py
+++ lsdo_rotor/core/BEM/functions/get_BEM_rotor_dictionary.py
@@ -1,9 +1,6 @@
 from lsdo_rotor.core.BEM.BEM_rotor_parameters import BEMRotorParameters
 import numpy as np
-from scipy.linalg import block_diag
 
-import sympy as sym 
-from sympy import *
 
 def get_BEM_rotor_dictionary(airfoil_name, interp, custom_polar):
    
--- lsdo_rotor/core/BILD/functions/get_BILD_rotor_dictionary.py
+++ lsdo_rotor/core/BILD/functions/get_BILD_rotor_dictionary.py
@@ -1,9 +1,5 @@
 from lsdo_rotor.core.BEM.BEM_rotor_parameters import BEMRotorParameters
-import numpy as np
-from scipy.linalg import block_diag
 
-import sympy as sym 
-from sympy import *
 
 def get_BILD_rotor_dictionary(airfoil_name, interp, custom_polar):
    
